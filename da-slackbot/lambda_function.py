49b3d34b9034977ac405813fbc19d49085f2b103
Kristoffer Bjärkefur <kbjarkefur@users.noreply.github.com>
2021-06-10 00:59:34 -0400

    Create lambda_function.py

---
+++ da-slackbot/lambda_function.py
@@ -0,0 +1,80 @@
+import json, ast, base64
+
+from urllib import parse
+
+
+def lambda_handler(event, context):
+    
+    print(event)
+    
+    if "teststr" in event:
+        event = ast.literal_eval(event["teststr"])
+    
+    
+    bodyEncStr = event["body"]
+    print(bodyEncStr)
+    
+    
+    params = str(base64.urlsafe_b64decode(bodyEncStr), "utf-8")
+    print(params)
+    
+    #Create a mock url to make params work with urllib
+    mock_url = "www.example.com/home?" + params
+    params = dict(parse.parse_qsl(parse.urlsplit(mock_url).query))
+    
+    user = params["user_name"]
+    
+    greeting = "Hello <@{}>, I am the *DIME Analytics* Slack bot :robot_face: - I am faster than any human in DIME Analytics. :woman-running::man-running:".format(user)
+    
+    #params_dict = dict(pair.split('=') for pair in params.split('&'))
+    print(params)
+    
+    #No subcommand used, show helpfile
+    if "text" not in params:
+        slack_message = """
+        
+        {} This is my helpfile. Here are the subcommands you can use:
+          - `/da github` - info related to GitHub and links to all guidlines for all actions related to the DIME and WB account  
+          - `/da scto` - info related to SurveyCTO.
+          
+        If you do not see a good option here, please email your question to dimeanalytics@worldbank.org.
+        
+        """.format(greeting)
+    
+    #Subcommand github used
+    elif params["text"] == "github":
+        slack_message = """
+        
+        {} Here are some *DIME Analytics* GitHub resources:
+          - Main page for all DIME Analytics GitHub guidelines: https://github.com/dime-worldbank/dime-account-admin
+        """.format(greeting)
+
+    #Subcommand github used
+    elif params["text"] == "scto":
+        slack_message = """
+        
+        {} Here are some *DIME Analytics* SurveyCTO resources:
+            - Create a new SurveyCTO server in the WB account (WB intranet only): https://worldbankgroup.service-now.com/wbg?id=wbg_sc_catalog&sys_id=7d1e71b86f16d340db112d232e3ee4aa
+            - Pause an existing SurveyCTO server in the WB account (WB intranet only): https://worldbankgroup.service-now.com/wbg?id=wbg_sc_catalog&sys_id=87ebb44ddbc1dc10d37979668c961931
+        """.format(greeting)
+
+    #No valid subcommand used
+    else :
+        slack_message = """
+            {} The subcommand subcommand `{}` does unfortunately not exist. Type `/da` for valid subcommands.
+        """.format(greeting,params["text"])
+
+    response_body = {
+        "response_type": "ephemeral",
+        "text": slack_message
+    }
+    
+    response_headers = {
+        "Content-Type": "application/json"
+    }
+
+    return {
+        "statusCode": 200,
+        "headers" : response_headers,
+        "body": json.dumps(response_body)
+    }
